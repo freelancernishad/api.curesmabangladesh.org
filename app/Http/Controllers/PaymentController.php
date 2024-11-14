3e8c62b1cacfa197961b8fe0a52165a31b5e5fbd
Nishad Hossain <freelancernishad123@gmail.com>
2024-11-14 17:07:21 +0600

    update

--- app/Http/Controllers/PaymentController.php
+++ app/Http/Controllers/PaymentController.php
@@ -17,7 +17,7 @@ class PaymentController extends Controller
     public function getPaymentsByDonationStatus(Request $request)
     {
         // Check for the 'withDonation' query parameter
-         $withDonation = $request->withDonation;
+         $DonationType = $request->DonationType;
 
         // Get the 'per_page' query parameter (default to 10 if not provided)
         $perPage = $request->query('per_page', 10);
@@ -26,12 +26,12 @@ class PaymentController extends Controller
         // Start the query for "Paid" payments
         $paymentsQuery = Payment::where('status', 'Paid');
 
-        // Apply the 'donate_for' filter based on the 'withDonation' query parameter
+        // Apply the 'donate_for' filter based on the 'DonationType' query parameter
 
-            if ($withDonation=='withuser') {
+            if ($DonationType=='withuser') {
                 // Include only "Paid" payments with a value in 'donate_for'
                 $paymentsQuery->whereNotNull('donate_for');
-            } elseif ($withDonation=='withoutuser')  {
+            } elseif ($DonationType=='withoutuser')  {
                 // Include only "Paid" payments without a value in 'donate_for'
                 $paymentsQuery->whereNull('donate_for');
             }
@@ -44,3 +44,5 @@ class PaymentController extends Controller
         return response()->json($payments);
     }
 }
+
+
